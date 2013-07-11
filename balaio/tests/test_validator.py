96eb9e01999129f496adb4126036510b326d2c20
Gustavo Fonseca <gustavofons@gmail.com>
2013-07-11 16:37:36 -0300

    Início da reescrita dos códigos do módulo validator
    Adicionados algumas docstrings e testes para a classe validator.ValidationPipe
    
    Removidos alguns lixos

--- balaio/tests/test_validator.py
+++ balaio/tests/test_validator.py
@@ -1,353 +1,95 @@
-import json
+# coding: utf-8
+import unittest
 import mocker
-from StringIO import StringIO
-from xml.etree.ElementTree import ElementTree
 
 from balaio import validator
-from balaio import notifier
+
+#
+# Stubs are test doubles to be used when the test does not aim
+# to check inner aspects of a collaboration.
+#
+class ScieloAPIClientStub(object):
+    def __init__(self, *args, **kwargs):
+        pass
 
 
-class FundingValidationPipeTest(mocker.MockerTestCase):
+class NotifierStub(object):
+    def __init__(self, *args, **kwargs):
+        pass
 
-    def _make_pipe(self, *args, **kwargs):
-        from balaio.validator import FundingValidationPipe
-        return FundingValidationPipe(*args, **kwargs)
+    def validation_event(self, *args, **kwargs):
+        pass
 
-    def _make_data(self, xml_string='<root><journal-title>Revista Brasileira ...</journal-title></root>'):
 
-        etree = ElementTree()
-        xml = etree.parse(StringIO(xml_string))
+#
+# Module level constants
+#
+class ConstantsTests(unittest.TestCase):
 
-        attempt = self.mocker.mock()
-        pkg_analyzer = self.mocker.mock()
+    def test_STATUS_OK_value(self):
+        self.assertEqual(validator.STATUS_OK, 'ok')
 
-        pkg_analyzer.xml
-        self.mocker.result(xml)
+    def test_STATUS_WARNING_value(self):
+        self.assertEqual(validator.STATUS_WARNING, 'warning')
 
-        return (attempt, pkg_analyzer)
+    def test_STATUS_ERROR_value(self):
+        self.assertEqual(validator.STATUS_ERROR, 'error')
 
-    def _validate(self, xml_string):
-        mock_manager = self.mocker.mock()
+
+#
+# Pipes
+#
+class ValidationPipeTests(mocker.MockerTestCase):
+
+    def test_scieloapi_arg_is_mandatory(self):
+        self.assertRaises(ValueError,
+                          lambda: validator.ValidationPipe([{'name': 'foo'}]))
+
+    def test_scieloapi_isnt_called_during_initialization(self):
+        mock_scieloapi = self.mocker.mock()
+        self.mocker.replay()
+
+        vpipe = validator.ValidationPipe([{'name': 'foo'}], scieloapi=mock_scieloapi)
+        self.assertIsInstance(vpipe, validator.ValidationPipe)
+
+    def test_notifier_is_called_during_initialization(self):
         mock_notifier = self.mocker.mock()
+        mock_scieloapi = self.mocker.mock()
 
         mock_notifier()
         self.mocker.result(mock_notifier)
 
-        mock_manager()
-        self.mocker.result(mock_manager)
-
-        data = self._make_data(xml_string)
         self.mocker.replay()
 
-        pipe = self._make_pipe(data, mock_manager, mock_notifier)
-        return pipe.validate(data[1])
+        vpipe = validator.ValidationPipe([{'name': 'foo'}],
+            scieloapi=mock_scieloapi, notifier_dep=mock_notifier)
 
-    def test_no_funding_group_and_no_ack(self):
-        expected = ['w', 'no funding-group and no ack']
+        self.assertIsInstance(vpipe, validator.ValidationPipe)
 
-        self.assertEquals(
-            expected,
-            self._validate('<root></root>'))
+    def test_transform_calls_validate_stage_notifier(self):
+        """
+        Asserts that methods expected to be defined by the subclasses are being called.
+        """
+        mock_self = self.mocker.mock(validator.ValidationPipe)
 
-    def test_no_funding_group_and_ack_has_no_number(self):
-        expected = ['ok', '<ack>acknowle<sub />dgements</ack>']
+        mock_self.validate('pkg_analyzer')
+        self.mocker.result(['ok', 'foo'])
 
-        self.assertEquals(
-            expected,
-            self._validate('<root><ack>acknowle<sub/>dgements</ack></root>'))
+        mock_self._stage_
+        self.mocker.result('bar')
 
-    def test_no_funding_group_and_ack_has_number(self):
-        expected = ['e', '<ack>acknowledgements<p>1234</p></ack>']
+        mock_self._notifier.validation_event(mocker.ANY)
+        self.mocker.result(None)
 
-        self.assertEquals(
-            expected,
-            self._validate('<root><ack>acknowledgements<p>1234</p></ack></root>'))
-
-    def test_funding_group(self):
-        expected = ['ok', '<funding-group>funding data</funding-group>']
-
-        self.assertEquals(expected, self._validate('<root><ack>acknowledgements<funding-group>funding data</funding-group></ack></root>'))
-
-
-class AbbrevJournalTitleValidationPipeTest(mocker.MockerTestCase):
-
-    def _make_pipe(self, *args, **kwargs):
-        from balaio.validator import AbbrevJournalTitleValidationPipe
-        return AbbrevJournalTitleValidationPipe(*args, **kwargs)
-
-    def _make_data(self, xml_string='<root><journal-title>Revista Brasileira ...</journal-title></root>'):
-
-        etree = ElementTree()
-        xml = etree.parse(StringIO(xml_string))
-
-        attempt = self.mocker.mock()
-        pkg_analyzer = self.mocker.mock()
-
-        pkg_analyzer.xml
-        self.mocker.result(xml)
-
-        pkg_analyzer.meta
-        self.mocker.result({'journal_title': 'Revista Brasileira ...'})
-
-        return (attempt, pkg_analyzer)
-
-    def _validate(self, xml_string, manager_result='{"journal-title":"Revista Brasileira ...", "title_iso": "Rev. Bras. ????"}'):
-        mock_manager = self.mocker.mock()
-        mock_notifier = self.mocker.mock()
-
-        mock_notifier()
-        self.mocker.result(mock_notifier)
-
-        mock_manager()
-        self.mocker.result(mock_manager)
-
-        mock_manager.journal('Revista Brasileira ...', 'title')
-        self.mocker.result(json.load(StringIO(manager_result)))
-
-        data = self._make_data(xml_string)
         self.mocker.replay()
 
-        pipe = self._make_pipe(data, mock_manager, mock_notifier)
-        return pipe.validate(data[1])
+        self.assertEqual(
+            validator.ValidationPipe.transform(mock_self, ['attempt', 'pkg_analyzer']),
+            ['attempt', 'pkg_analyzer'])
 
-    def test_abbrev_journal_title_is_valid(self):
-        expected = ['ok', 'Rev. Bras. ????']
+    def test_validate_raises_NotImplementedError(self):
+        vpipe = validator.ValidationPipe([{'name': 'foo'}],
+            scieloapi=ScieloAPIClientStub(), notifier_dep=NotifierStub)
 
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><abbrev-journal-title abbrev-type="publisher">Rev. Bras. ????</abbrev-journal-title></journal-meta></root>'))
+        self.assertRaises(NotImplementedError, lambda: vpipe.validate('foo'))
 
-    def test_abbrev_journal_title_not_found_in_xml(self):
-        expected = ['e', './/journal-meta/abbrev-journal-title[@abbrev-type="publisher"] not found in XML']
-        self.assertEquals(
-            expected,
-            self._validate('<root><abbrev-journal-title>titulo abreviado</abbrev-journal-title></root>'))
-
-    def test_abbrev_journal_title_not_found_in_manager(self):
-        expected = ['e', 'title_iso not found in Manager']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><abbrev-journal-title abbrev-type="publisher">Rev. Bras. ????</abbrev-journal-title></journal-meta></root>', '{"journal-title":"Revista Brasileira ..."}'))
-
-    def test_abbrev_journal_title_not_matched(self):
-        expected = ['e', u'Data in XML and Manager do not match.' + '\n' + 'Data in Manager: Rev. Bras. ????' + '\n' + 'Data in XML: Rev Bras ????']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><abbrev-journal-title abbrev-type="publisher">Rev Bras ????</abbrev-journal-title></journal-meta></root>'))
-
-
-class ISSNCheckingPipeTest(mocker.MockerTestCase):
-
-    def _make_pipe(self, *args, **kwargs):
-        from balaio.validator import ISSNValidationPipe
-        return ISSNValidationPipe(*args, **kwargs)
-
-    def _make_data(self, xml_string='<root></root>'):
-        etree = ElementTree()
-        xml = etree.parse(StringIO(xml_string))
-
-        attempt = self.mocker.mock()
-        pkg_analyzer = self.mocker.mock()
-
-        pkg_analyzer.xml
-        self.mocker.result(xml)
-
-        return (attempt, pkg_analyzer)
-
-    def _validate(self, xml_string):
-        mock_manager = self.mocker.mock()
-        mock_notifier = self.mocker.mock()
-
-        mock_notifier()
-        self.mocker.result(mock_notifier)
-
-        mock_manager()
-        self.mocker.result(mock_manager)
-
-        data = self._make_data(xml_string)
-        self.mocker.replay()
-
-        pipe = self._make_pipe(data, mock_manager, mock_notifier)
-        return pipe.validate(data[1])
-
-    def test_pipe_issn_with_one_valid_ISSN(self):
-        expected = ['ok', '']
-
-        self.assertEquals(
-            self._validate("<root><issn pub-type='epub'>0102-6720</issn></root>"), expected)
-
-    def test_pipe_issn_with_one_invalid_ISSN(self):
-        expected = ['e', 'neither eletronic ISSN nor print ISSN are valid']
-
-        self.assertEquals(
-            self._validate("<root><issn pub-type='ppub'>1234-1234</issn></root>"), expected)
-
-    def test_pipe_issn_with_two_valid_ISSN_eletronic_and_print(self):
-        expected = ['ok', '']
-
-        self.assertEquals(
-            self._validate("<root><issn pub-type='ppub'>0100-879X</issn><issn pub-type='epub'>1414-431X</issn></root>"), expected)
-
-    def test_pipe_issn_with_strange_ISSN(self):
-        expected = ['e', 'neither eletronic ISSN nor print ISSN are valid']
-
-        self.assertEquals(
-            self._validate("<root><issn pub-type='ppub'>01ols0-OIN</issn></root>"), expected)
-
-    def test_pipe_issn_with_one_strange_ISSN_and_one_valid_ISSN(self):
-        expected = ['ok', '']
-
-        self.assertEquals(
-            self._validate("<root><issn pub-type='ppub'>01ols0-OIN</issn><issn pub-type='epub'>1414-431X</issn></root>"), expected)
-
-
-class NLMJournalTitleValidationPipeTest(mocker.MockerTestCase):
-
-    def _make_pipe(self, *args, **kwargs):
-        from balaio.validator import NLMJournalTitleValidationPipe
-        return NLMJournalTitleValidationPipe(*args, **kwargs)
-
-    def _make_data(self, xml_string='<root><journal-title>Revista Brasileira ...</journal-title></root>'):
-
-        etree = ElementTree()
-        xml = etree.parse(StringIO(xml_string))
-
-        attempt = self.mocker.mock()
-        pkg_analyzer = self.mocker.mock()
-
-        pkg_analyzer.xml
-        self.mocker.result(xml)
-
-        pkg_analyzer.meta
-        self.mocker.result({'journal_title': 'Revista Brasileira ...'})
-
-        return (attempt, pkg_analyzer)
-
-    def _validate(self, xml_string, manager_result='{"journal-title":"Revista Brasileira ...", "medline_title": "Rev. Bras. ????"}'):
-        mock_manager = self.mocker.mock()
-        mock_notifier = self.mocker.mock()
-
-        mock_notifier()
-        self.mocker.result(mock_notifier)
-
-        mock_manager()
-        self.mocker.result(mock_manager)
-
-        mock_manager.journal('Revista Brasileira ...', 'title')
-        self.mocker.result(json.load(StringIO(manager_result)))
-
-        data = self._make_data(xml_string)
-        self.mocker.replay()
-
-        pipe = self._make_pipe(data, mock_manager, mock_notifier)
-        return pipe.validate(data[1])
-
-    def test_nlm_journal_title_is_valid(self):
-        expected = ['ok', 'Rev. Bras. ????']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><journal-id journal-id-type="nlm-ta">Rev. Bras. ????</journal-id></journal-meta></root>'))
-
-    def test_nlm_journal_not_found_in_xml_and_not_found_in_manager(self):
-        expected = ['ok', '']
-        self.assertEquals(
-            expected,
-            self._validate('<root><abbrev-journal-title>titulo abreviado</abbrev-journal-title></root>', '{"journal-title":"Revista Brasileira ..."}'))
-
-    def test_nlm_journal_title_not_found_in_xml(self):
-        expected = ['e', './/journal-meta/journal-id[@journal-id-type="nlm-ta"] not found in XML']
-        self.assertEquals(
-            expected,
-            self._validate('<root><abbrev-journal-title>titulo abreviado</abbrev-journal-title></root>'))
-
-    def test_nlm_journal_title_not_found_in_manager(self):
-        expected = ['e', 'medline_title not found in Manager']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><journal-id journal-id-type="nlm-ta">Rev. Bras. ????</journal-id></journal-meta></root>', '{"journal-title":"Revista Brasileira ..."}'))
-
-    def test_nlm_journal_title_not_matched(self):
-        expected = ['e', u'Data in XML and Manager do not match.' + '\n' + 'Data in Manager: Rev. Bras. ????' + '\n' + 'Data in XML: Rev Bras ????']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><journal-id journal-id-type="nlm-ta">Rev Bras ????</journal-id></journal-meta></root>'))
-
-
-class PublisherNameValidationPipeTest(mocker.MockerTestCase):
-
-    def _make_pipe(self, *args, **kwargs):
-        from balaio.validator import PublisherNameValidationPipe
-        return PublisherNameValidationPipe(*args, **kwargs)
-
-    def _make_data(self, xml_string='<root><journal-title>Revista Brasileira ...</journal-title></root>'):
-
-        etree = ElementTree()
-        xml = etree.parse(StringIO(xml_string))
-
-        attempt = self.mocker.mock()
-        pkg_analyzer = self.mocker.mock()
-
-        pkg_analyzer.xml
-        self.mocker.result(xml)
-
-        pkg_analyzer.meta
-        self.mocker.result({'journal_title': 'Revista Brasileira ...'})
-
-        return (attempt, pkg_analyzer)
-
-    def _validate(self, xml_string, manager_result='{"journal-title":"Revista Brasileira ...", "publisher_name": "Publicador ????"}'):
-        mock_manager = self.mocker.mock()
-        mock_notifier = self.mocker.mock()
-
-        mock_notifier()
-        self.mocker.result(mock_notifier)
-
-        mock_manager()
-        self.mocker.result(mock_manager)
-
-        mock_manager.journal('Revista Brasileira ...', 'title')
-        self.mocker.result(json.load(StringIO(manager_result)))
-
-        data = self._make_data(xml_string)
-        self.mocker.replay()
-
-        pipe = self._make_pipe(data, mock_manager, mock_notifier)
-        return pipe.validate(data[1])
-
-    def test_publisher_name_is_valid(self):
-        expected = ['ok', 'Publicador   ????']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><publisher><publisher-name>Publicador   ????</publisher-name></publisher></journal-meta></root>'))
-
-    def test_publisher_name_not_found_in_xml_and_not_found_in_manager(self):
-        expected = ['e', 'Both publisher_name in Manager and .//journal-meta/publisher/publisher-name in XML are mandatory. But both are missing.']
-        self.assertEquals(
-            expected,
-            self._validate('<root><abbrev-journal-title>titulo abreviado</abbrev-journal-title></root>', '{"journal-title":"Revista Brasileira ..."}'))
-
-    def test_publisher_name_not_found_in_xml(self):
-        expected = ['e', './/journal-meta/publisher/publisher-name not found in XML']
-        self.assertEquals(
-            expected,
-            self._validate('<root><abbrev-journal-title>titulo abreviado</abbrev-journal-title></root>'))
-
-    def test_publisher_name_not_found_in_manager(self):
-        expected = ['e', 'publisher_name not found in Manager']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><publisher><publisher-name>Publicador   ????</publisher-name></publisher></journal-meta></root>', '{"journal-title":"Revista Brasileira ..."}'))
-
-    def test_publisher_name_not_matched(self):
-        expected = ['e', u'Data in XML and Manager do not match.' + '\n' + 'Data in Manager: Publicador ????' + '\n' + 'Data in XML: Publ~icador   ??***??']
-
-        self.assertEquals(
-            expected,
-            self._validate('<root><journal-meta><publisher><publisher-name>Publ~icador   ??***??</publisher-name></publisher></journal-meta></root>'))
--- balaio/validator.py
+++ balaio/validator.py
@@ -1,34 +1,42 @@
 # coding: utf-8
-import sys
-import utils
-import plumber
-import notifier
 import scieloapi
-import xml.etree.ElementTree as etree
+import plumber
 
-from utils import Configuration
+import utils
+import notifier
 
-config = Configuration.from_env()
+#config = utils.Configuration.from_env()
 
 STATUS_OK = 'ok'
-STATUS_WARNING = 'w'
-STATUS_ERROR = 'e'
+STATUS_WARNING = 'warning'
+STATUS_ERROR = 'error'
 
 
 class ValidationPipe(plumber.Pipe):
     """
-    Specialized Pipe which validates the data and notifies the result
+    Specialized Pipe which validates the data and notifies the result.
     """
-    def __init__(self, data, manager_dep=scieloapi.Manager, notifier_dep=notifier.Notifier):
+    def __init__(self, data, scieloapi=None, notifier_dep=notifier.Notifier):
+        """
+        `data` is an iterable that will pass thru the pipe.
+        `scieloapi` is an instance of scieloapi.Client.
+        """
         super(ValidationPipe, self).__init__(data)
+
         self._notifier = notifier_dep()
-        self._manager = manager_dep()
+        if scieloapi:
+            self._scieloapi = scieloapi
+        else:
+            raise ValueError('missing argument scieloapi')
 
-    def transform(self, data):
-        # data = (Attempt, PackageAnalyzer)
-        # PackagerAnalyzer.xml
-        attempt, package_analyzer = data
+    def transform(self, item):
+        """
+        Performs a transformation to one `item` of data iterator.
 
+        `item` is a pair comprised of instances of models.Attempt
+        and checkin.PackageAnalyzer.
+        """
+        attempt, package_analyzer = item
         result_status, result_description = self.validate(package_analyzer)
 
         message = {
@@ -39,191 +47,14 @@ class ValidationPipe(plumber.Pipe):
 
         self._notifier.validation_event(message)
 
-        return data
+        return item
 
-    def compare_registered_data_and_xml_data(self, package_analyzer, mandatory=False):
+    def validate(self, package):
         """
-        Compares the data registered in SciELO Manager and the data in XML ignoring blank spaces and lower/uppercase
-        Returns [status, description]
+        Performs the validation of `package`.
+
+        `package` is a checkin.PackageAnalyzer instance,
+        representing the article package under validation.
         """
-        registered_data = self._registered_data(package_analyzer)
-        xml_data = self._xml_data(package_analyzer)
+        raise NotImplementedError()
 
-        if registered_data is None and xml_data == '':
-            status, description = [STATUS_ERROR, 'Both ' + self._registered_data_label + ' in Manager and ' + self._xml_data_label + ' in XML are mandatory. But both are missing.'] if mandatory is True else [STATUS_OK, xml_data]
-        elif registered_data is None:
-            status, description = [STATUS_ERROR, self._registered_data_label + ' not found in Manager']
-        elif xml_data == '':
-            status, description = [STATUS_ERROR, self._xml_data_label + ' not found in XML']
-        elif xml_data.lower().replace(' ', '') == registered_data.lower().replace(' ', ''):
-            status, description = [STATUS_OK, xml_data]
-        else:
-            status = STATUS_ERROR
-            description = 'Data in XML and Manager do not match.' + '\n' + 'Data in Manager: ' + registered_data + '\n' + 'Data in XML: ' + xml_data
-            # + '\n' + xml_data.lower().replace(' ', '-') + '\n' + registered_data.lower().replace(' ', '-')
-        return [status, description]
-
-
-# Pipes to validate journal data
-class ISSNValidationPipe(ValidationPipe):
-    """
-    Verify if the ISSN(s) exist on SciELO scieloapi.Manager and if it`s a valid ISSN.
-
-    Analyzed atribute from ``.//issn``: ``@pub-type="ppub"`` or ``@pub-type="epub"``
-
-    """
-    _stage_ = 'issn'
-
-    def validate(self, package_analyzer):
-
-        data = package_analyzer.xml
-
-        journal_eissn = data.findtext(".//issn[@pub-type='epub']")
-        journal_pissn = data.findtext(".//issn[@pub-type='ppub']")
-
-        if utils.is_valid_issn(journal_pissn) or utils.is_valid_issn(journal_eissn):
-            if journal_pissn:
-                #Validate journal_pissn against SciELO scieloapi.Manager
-                pass
-            if journal_eissn:
-                #Validate journal_eissn against SciELO scieloapi.Manager
-                pass
-            return [STATUS_OK, '']
-        else:
-            return [STATUS_ERROR, 'neither eletronic ISSN nor print ISSN are valid']
-
-
-class AbbrevJournalTitleValidationPipe(ValidationPipe):
-    """
-    Check if journal-meta/abbrev-journal-title[@abbrev-type='publisher'] and the data registered in SciELO Manager matches
-    """
-    def validate(self, package_analyzer):
-        # set registered data label in order to display it in description
-        self._registered_data_label = 'title_iso'
-        # set xml data label (xpath) in order to display it in description
-        self._xml_data_label = './/journal-meta/abbrev-journal-title[@abbrev-type="publisher"]'
-        return self.compare_registered_data_and_xml_data(package_analyzer, mandatory=True)
-
-    def _xml_data(self, package_analyzer):
-        """
-        Returns the content of ``self._xml_data_label`` (xpath in XML)
-        """
-        node = package_analyzer.xml.find(self._xml_data_label)
-        return node.text if not node is None else ''
-
-    def _registered_data(self, package_analyzer):
-        """
-        Returns the data registered in SciELO Manager
-        Gets a single journal by ``package_analyzer.meta[journal_title]``
-        then returns the registered data identified by ``self.registered_data_label``
-        """
-        # gets a single journal by ``package_analyzer.meta[journal_title]``
-        journal_data = self._manager.journal(package_analyzer.meta['journal_title'], 'title')
-        # returns the registered data identified by ``self._registered_data_label``
-        return journal_data.get(self._registered_data_label, None)
-
-
-class NLMJournalTitleValidationPipe(ValidationPipe):
-    """
-    Check if journal-meta/journal-id[@journal-id-type='nlm-ta'] and the data registered in SciELO Manager matches
-    """
-    def validate(self, package_analyzer):
-        # set registered data label in order to display it in description
-        self._registered_data_label = 'medline_title'
-        # set xml data label (xpath) in order to display it in description
-        self._xml_data_label = './/journal-meta/journal-id[@journal-id-type="nlm-ta"]'
-        return self.compare_registered_data_and_xml_data(package_analyzer)
-
-    def _xml_data(self, package_analyzer):
-        """
-        Returns the content of ``self._xml_data_label`` (xpath in XML)
-        """
-        node = package_analyzer.xml.find(self._xml_data_label)
-        return node.text if not node is None else ''
-
-    def _registered_data(self, package_analyzer):
-        """
-        Returns the data registered in SciELO Manager
-        Gets a single journal by ``package_analyzer.meta[journal_title]``
-        then returns the registered data identified by ``self.registered_data_label``
-        """
-        # gets a single journal by ``package_analyzer.meta[journal_title]``
-        journal_data = self._manager.journal(package_analyzer.meta['journal_title'], 'title')
-        # returns the registered data identified by ``self._registered_data_label``
-        return journal_data.get(self._registered_data_label, None)
-
-
-class PublisherNameValidationPipe(ValidationPipe):
-    """
-    Check if journal-meta/publisher/publisher-name and the data registered in SciELO Manager matches
-    """
-    def validate(self, package_analyzer):
-        # FIXME se varios publisher_name, qual seria o separador? quebra de linha?
-        # set registered data label in order to display it in description
-        self._registered_data_label = 'publisher_name'
-        # set xml data label (xpath) in order to display it in description
-        self._xml_data_label = './/journal-meta/publisher/publisher-name'
-        return self.compare_registered_data_and_xml_data(package_analyzer, mandatory=True)
-
-    def _xml_data(self, package_analyzer):
-        """
-        Returns the content of ``self._xml_data_label`` (xpath in XML)
-        """
-        node = package_analyzer.xml.find(self._xml_data_label)
-        return node.text if not node is None else ''
-
-    def _registered_data(self, package_analyzer):
-        """
-        Returns the data registered in SciELO Manager
-        Gets a single journal by ``package_analyzer.meta[journal_title]``
-        then returns the registered data identified by ``self.registered_data_label``
-        """
-        # gets a single journal by ``package_analyzer.meta[journal_title]``
-        journal_data = self._manager.journal(package_analyzer.meta['journal_title'], 'title')
-        # returns the registered data identified by ``self._registered_data_label``
-        return journal_data.get(self._registered_data_label, None)
-
-# Pipes to validate issue data
-
-
-# Pipes to validate article data
-class FundingValidationPipe(ValidationPipe):
-    """
-    Check the absence/presence of funding-group and ack in the document
-
-    funding-group is a mandatory element only if there is contract or project number
-    in the document. Sometimes this information comes in Acknowledgments section.
-    Return
-    [STATUS_ERROR, ack]           if no founding-group, but Acknowledgments (ack) has number
-    [STATUS_OK, founding-group]   if founding-group is present
-    [STATUS_OK, ack]              if no founding-group, but Acknowledgments has no numbers
-    [STATUS_WARNING, 'no funding-group and no ack'] if founding-group and Acknowledgments (ack) are absents
-    """
-    _stage_ = 'funding-group'
-
-    def validate(self, package_analyzer):
-
-        data = package_analyzer.xml
-
-        funding_nodes = data.findall('.//funding-group')
-
-        status, description = [STATUS_OK, etree.tostring(funding_nodes[0])] if funding_nodes != [] else [STATUS_WARNING, 'no funding-group']
-        if not status == STATUS_OK:
-            ack_node = data.findall('.//ack')
-            description = etree.tostring(ack_node[0]) if ack_node != [] else 'no funding-group and no ack'
-            status = STATUS_ERROR if self._contains_number(description) else STATUS_OK if description != 'no funding-group and no ack' else STATUS_WARNING
-        return [status, description]
-
-    def _contains_number(self, text):
-        # if text contains any number
-        return any((True for n in xrange(10) if str(n) in text))
-
-
-ppl = plumber.Pipeline(ISSNValidationPipe, AbbrevJournalTitleValidationPipe, NLMJournalTitleValidationPipe, FundingValidationPipe)
-
-if __name__ == '__main__':
-    messages = utils.recv_messages(sys.stdin, utils.make_digest)
-    try:
-        results = [msg for msg in ppl.run(messages)]
-    except KeyboardInterrupt:
-        sys.exit(0)
