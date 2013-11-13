9ed4ce3071f5aeed2b94184de53135d1a21a81fa
Gustavo Fonseca <gustavofons@gmail.com>
2013-11-13 15:46:52 -0200

    Ajustes na configuração do travis-ci para utilizar postgres

---
+++ balaio/tests/modelfactories.py
@@ -0,0 +1,43 @@
+import factory
+from factory.alchemy import SQLAlchemyModelFactory
+
+from balaio import models
+
+
+class ArticlePkgFactory(SQLAlchemyModelFactory):
+    FACTORY_FOR = models.ArticlePkg
+    FACTORY_SESSION = models.ScopedSession
+
+    id = factory.Sequence(lambda n: n)
+    article_title = 'Construction of a recombinant adenovirus...'
+    journal_pissn = '0100-879X'
+    journal_eissn = '0100-879X'
+    journal_title = 'Associa... Brasileira'
+    issue_year = 1995
+    issue_volume = '67'
+    issue_number = '8'
+    issue_suppl_volume = None
+    issue_suppl_number = None
+
+
+class AttemptFactory(SQLAlchemyModelFactory):
+    FACTORY_FOR = models.Attempt
+    FACTORY_SESSION = models.ScopedSession
+
+    id = factory.Sequence(lambda n: n)
+    package_checksum = factory.Sequence(lambda n: '20132df0as89dds73as936%s' % n)
+    finished_at = None
+    collection_uri = ''
+    articlepkg = factory.SubFactory(ArticlePkgFactory)
+    filepath = '/tmp/watch/xxx.zip'
+
+
+class TicketFactory(SQLAlchemyModelFactory):
+    FACTORY_FOR = models.Ticket
+    FACTORY_SESSION = models.ScopedSession
+
+    id = factory.Sequence(lambda n: n)
+    title = "Erro no pacote xxx"
+    author = 'Aberlado Barbosa'
+    articlepkg = factory.SubFactory(ArticlePkgFactory)
+
