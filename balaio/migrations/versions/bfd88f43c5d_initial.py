44e97c9345ff099c9080c43f32d3a95459ecf348
Jamil Atta Junior <atta.jamil@gmail.com>
2014-02-07 16:33:35 -0200

    Adicionado a migração initial, migração do attempt e alterações no env.py para que o alembic possa realizar comparação com o banco de dados

---
+++ balaio/migrations/versions/bfd88f43c5d_initial.py
@@ -0,0 +1,22 @@
+"""Initial
+
+Revision ID: bfd88f43c5d
+Revises: None
+Create Date: 2014-02-07 15:41:39.908961
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'bfd88f43c5d'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
